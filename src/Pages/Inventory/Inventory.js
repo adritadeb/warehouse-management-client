0a4b3b4f98de71db6a8c7fdfde2c4fdfe38a85ec
adritadeb <adritad2019@gmail.com>
2022-05-08 01:27:44 +0600

    decrease item quantity implemented

--- src/Pages/Inventory/Inventory.js
+++ src/Pages/Inventory/Inventory.js
@@ -1,27 +1,60 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
 import { Link, useParams } from "react-router-dom";
 import { toast } from 'react-toastify';
 
 const Inventory = () => {
     const { id } = useParams();
     const [book, setBook] = useState({});
+    const [itemQuantity, setItemQuantity] = useState(0);
 
+    //Load book data with id
     useEffect(() => {
         const url = `http://localhost:5000/inventory/${id}`;
         fetch(url)
             .then(res => res.json())
             .then(data => setBook(data));
-    }, []);
+    }, [itemQuantity]);
 
-    const { img, name, supplier, body, quantity, price } = book;
+    let { img, name, supplier, body, quantity, price } = book;
 
-    const [itemQuantity, setItemQuantity] = useState('');
-
-    const increaseQuantity = event => {
-        event.preventDefault();
-        const quantity = event.target.quantity.value;
+    //Decrease item quantity
+    const decreaseQuantity = () => {
+        if (quantity === 0) {
+            toast('Item is stocked out');
+            return;
+        }
+        const newQuantity = quantity - 1;
+        quantity = newQuantity;
         const updatedQuantity = { quantity };
 
+        const url = `http://localhost:5000/inventory/${id}`;
+        fetch(url, {
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(updatedQuantity)
+        })
+            .then(res => res.json())
+            .then(data => {
+                setItemQuantity(updatedQuantity.quantity);
+                toast('Item delivered successfully');
+            });
+    };
+
+    //Increase item quantity
+    const increaseQuantity = event => {
+        event.preventDefault();
+        const inputQuantity = event.target.quantity.value;
+
+        if (!inputQuantity) {
+            toast('Please give a restock number');
+            return;
+        }
+
+        const newQuantity = quantity + parseInt(inputQuantity)
+        quantity = newQuantity;
+        const updatedQuantity = { quantity };
         const url = `http://localhost:5000/inventory/${id}`;
         fetch(url, {
             method: 'PUT',
@@ -36,7 +69,7 @@ const Inventory = () => {
                 toast('Item stocked successfully');
                 event.target.reset();
             });
-    }
+    };
 
 
     return (
@@ -50,10 +83,8 @@ const Inventory = () => {
                         <h4>Supplier: {supplier}</h4>
                         <p>{body}</p>
                         <div className='d-flex justify-content-between'>
-                            <h4>Quantity: {
-                                itemQuantity ? itemQuantity : quantity
-                            }</h4>
-                            <input className='btn btn-primary border-0' type="submit" value="Delivered" />
+                            <h4>Quantity: {itemQuantity ? itemQuantity : quantity}</h4>
+                            <input onClick={decreaseQuantity} className='btn btn-primary border-0' type="submit" value="Delivered" />
                         </div>
                         <h3>Price: ${price}</h3>
                     </div>
@@ -61,9 +92,9 @@ const Inventory = () => {
 
                 {/* Item restock section */}
                 <div className='border border-2 border-secondary rounded-3 m-5 p-3 w-md-25 h-50'>
-                    <h4 className='text-center'>Restock the items</h4>
+                    <h4 className='text-center'>Restock items</h4>
                     <form onSubmit={increaseQuantity}>
-                        <input className='my-4 ms-4 w-75' placeholder='Put a number' type="number" name="quantity" />
+                        <input className='my-4 ms-4 w-75' placeholder='Restock number' type="number" name="quantity" />
                         <br />
                         <input className='btn btn-primary mx-auto w-100' type="submit" value="Stock" />
                     </form>
