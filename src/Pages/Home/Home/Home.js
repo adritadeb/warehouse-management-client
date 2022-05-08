598579f095031f09af0f5c91e7701e9dd492b3eb
adritadeb <adritad2019@gmail.com>
2022-05-08 17:00:41 +0600

    second extra section done

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Banner from '../Banner/Banner';
 import Books from '../Books/Books';
 import NewBooks from '../NewBooks/NewBooks';
+import Review from '../Review/Review';
 
 const Home = () => {
     return (
@@ -9,6 +10,7 @@ const Home = () => {
             <Banner></Banner>
             <Books></Books>
             <NewBooks></NewBooks>
+            <Review></Review>
         </div>
     );
 };
---
+++ src/Pages/Home/Review/Review.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Review = () => {
+    return (
+        <div className='w-50 mx-auto text-center'>
+            <h2>Hello Users</h2>
+            <p className='mb-1 mt-4 text-secondary'>Your feedback is valuable for us. Let us know what your think about us. Tell us good, bad or whatever you think. So that, we can improve more.</p>
+            <p className='mb-2 text-secondary'>Give your feedback on our support mail -</p>
+            <h5 className='text-primary'>BooksStocker21@gmail.com</h5>
+            <h4 className='my-4'>Contact with us : 01817822222, 01813334442</h4>
+        </div>
+    );
+};
+
+export default Review;
